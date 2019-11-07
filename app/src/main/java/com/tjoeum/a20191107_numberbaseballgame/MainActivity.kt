3d33facb318860d9eaf68dfff081066d7768240d
Minsung89 <minsung0217@gmail.com>
2019-11-07 20:07:58 +0900

    문제출제했다

--- app/src/main/java/com/tjoeum/a20191107_numberbaseballgame/MainActivity.kt
+++ app/src/main/java/com/tjoeum/a20191107_numberbaseballgame/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.tjoeum.a20191107_numberbaseballgame
 
 import android.os.Bundle
+import android.util.Log
+import kotlin.random.Random
 
 class MainActivity : BaseActivity() {
+
+
+    var questionNumArray = ArrayList<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -10,14 +15,30 @@ class MainActivity : BaseActivity() {
 
         setupEvents()
         setValues()
+        createQuestion()
+    }
+
+    fun createQuestion(){
+        while (true) {
+            val number = Random.nextInt(1, 9)
+            if(!questionNumArray.contains(number))
+                questionNumArray.add(number)
+            if(questionNumArray.size == 3)
+                break
+
+        }
+
+        for (num in questionNumArray){
+            Log.i("번호",num.toString())
+        }
+
 
     }
+
     override fun setupEvents() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun setValues() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
 
