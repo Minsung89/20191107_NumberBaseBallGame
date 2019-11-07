a43046bbdda5785fe2d6e5fbbed0602418f9c7c4
Minsung89 <minsung0217@gmail.com>
2019-11-07 20:15:24 +0900

    데이터 클래스 생성

---
+++ app/src/main/java/com/tjoeum/a20191107_numberbaseballgame/datas/ChatData.kt
@@ -0,0 +1,12 @@
+package com.tjoeum.a20191107_numberbaseballgame.datas
+
+import java.io.Serializable
+
+class ChatData(message:String, speaker:String) : Serializable{
+
+    var message = message
+    var speaker = speaker
+
+
+
+}
